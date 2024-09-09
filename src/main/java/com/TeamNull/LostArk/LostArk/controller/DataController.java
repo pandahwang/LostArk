5bbb29aab15da2df5d939073a13159d39b8d2585
easys <doit@easys.co.kr>
2024-09-09 17:52:04 +0900

    DataController 클래스에서 read 메서드 생성

--- src/main/java/com/TeamNull/LostArk/LostArk/controller/DataController.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/controller/DataController.java
@@ -1,4 +1,19 @@
 package com.TeamNull.LostArk.LostArk.controller;
 
+import com.TeamNull.LostArk.LostArk.dto.DataDto;
+import com.TeamNull.LostArk.LostArk.service.DataService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class DataController {
+
+    private final DataService dataService;
+
+    @GetMapping("/statistics/data")
+    public DataDto read (int id) {
+        return dataService.findById(id);
+    }
 }
