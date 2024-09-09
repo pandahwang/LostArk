e9b337011b723bc4592da5d8a36874c072cc096d
qnak9740 <qnak9740@naver.com>
2024-09-09 18:51:15 +0900

    Dto 작성

--- src/main/java/com/TeamNull/LostArk/LostArk/dto/ResultDto.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/dto/ResultDto.java
@@ -1,4 +1,18 @@
 package com.TeamNull.LostArk.LostArk.dto;
 
+import com.TeamNull.LostArk.LostArk.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResultDto {
+
+    private int id;
+    private User user;
+    private String topFactor1;
+    private String topFactor2;
+    private String topFactor3;
+    private String topFactor4;
+    private String topFactor5;
 }
