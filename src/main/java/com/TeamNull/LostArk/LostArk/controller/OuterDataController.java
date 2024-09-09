0da72fae103c567cee2524c361b64fadc27818dd
pandahwang <pandahwang@naver.com>
2024-09-09 17:42:00 +0900

    alluser() api로부터 가져온 데이터를 json 객체로 반환하도록 수정

--- src/main/java/com/TeamNull/LostArk/LostArk/controller/OuterDataController.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/controller/OuterDataController.java
@@ -2,6 +2,9 @@ package com.TeamNull.LostArk.LostArk.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,16 +19,21 @@ import java.net.URL;
 @RequiredArgsConstructor
 public class OuterDataController {
 
+
     // 목표 : Get 요청 시 api로부터 데이터를 받아와 저장 후 반환하도록 구현.
     @GetMapping("/statistic/alluser")
-    public String alluser() throws IOException {
+    public JSONObject alluser() throws IOException, ParseException {
         String result;
         URL url = new URL("https://asia-northeast3-loasearch.cloudfunctions.net/app/total?minLv=&maxLv=");
         BufferedReader bf;
         bf = new BufferedReader(new InputStreamReader(url.openStream(),"UTF-8"));
         result = bf.readLine();
 
-        return result;
+        // 받아온 데이터를 JSON으로 변환
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject)jsonParser.parse(result);
+
+        return jsonObject;
     }
 
 
