3f5f27ac106f406920dc53fca771e6ed75d469d8
pandahwang <pandahwang@naver.com>
2024-09-09 17:36:18 +0900

    /statistic/alluser로 요청 시 api로부터 가져온 데이터를 받아 문자열로 반환하도록 구현

--- src/main/java/com/TeamNull/LostArk/LostArk/controller/OuterDataController.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/controller/OuterDataController.java
@@ -1,4 +1,32 @@
 package com.TeamNull.LostArk.LostArk.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONArray;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+@RestController
+@RequiredArgsConstructor
 public class OuterDataController {
+
+    // 목표 : Get 요청 시 api로부터 데이터를 받아와 저장 후 반환하도록 구현.
+    @GetMapping("/statistic/alluser")
+    public String alluser() throws IOException {
+        String result;
+        URL url = new URL("https://asia-northeast3-loasearch.cloudfunctions.net/app/total?minLv=&maxLv=");
+        BufferedReader bf;
+        bf = new BufferedReader(new InputStreamReader(url.openStream(),"UTF-8"));
+        result = bf.readLine();
+
+        return result;
+    }
+
+
 }
