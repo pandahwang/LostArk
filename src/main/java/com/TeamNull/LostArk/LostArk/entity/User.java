ec41fcf17ad635a05ddeb21e5a0d42b1b5507af1
MOON1106 <mlnwoun8@gmail.com>
2024-09-09 17:41:13 +0900

    Entity클래스 ID값 UUID타입 지정

--- src/main/java/com/TeamNull/LostArk/LostArk/entity/User.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/entity/User.java
@@ -3,6 +3,7 @@ package com.TeamNull.LostArk.LostArk.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Check;
 
 import java.util.UUID;
 
@@ -12,38 +13,48 @@ import java.util.UUID;
 @Setter
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID", columnDefinition = "BINARY(16)")
     private UUID id;
 
     @Column(name = "Question1")
+    @Check(constraints = "Question BETWEEN 1 AND 5")
     private int question1;
 
     @Column(name = "Question2")
+    @Check(constraints = "Question BETWEEN 1 AND 5")
     private int question2;
 
     @Column(name = "Question3")
+    @Check(constraints = "Question BETWEEN 1 AND 5")
     private int question3;
 
     @Column(name = "Question4")
+    @Check(constraints = "Question BETWEEN 1 AND 5")
     private int question4;
 
     @Column(name = "Question5")
+    @Check(constraints = "Question BETWEEN 1 AND 5")
     private int question5;
 
     @Column(name = "Question6")
+    @Check(constraints = "Question BETWEEN 1 AND 5")
     private int question6;
 
     @Column(name = "Question7")
+    @Check(constraints = "Question BETWEEN 1 AND 5")
     private int question7;
 
     @Column(name = "Question8")
+    @Check(constraints = "Question BETWEEN 1 AND 5")
     private int question8;
 
     @Column(name = "Question9")
+    @Check(constraints = "Question BETWEEN 1 AND 5")
     private int question9;
 
     @Column(name = "Question10")
+    @Check(constraints = "Question BETWEEN 1 AND 5")
     private int question10;
 
 }
