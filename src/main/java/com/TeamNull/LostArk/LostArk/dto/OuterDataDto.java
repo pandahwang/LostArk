32e3f8ffbd8f6987cb717741343ab1dc379fc602
pandahwang <pandahwang@naver.com>
2024-09-09 17:27:30 +0900

    OuterDataDTO 필드 및 어노테이션 추가

--- src/main/java/com/TeamNull/LostArk/LostArk/dto/OuterDataDto.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/dto/OuterDataDto.java
@@ -1,4 +1,39 @@
 package com.TeamNull.LostArk.LostArk.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
 public class OuterDataDto {
+    private int id;
+    private int berserker;
+    private int destroyer;
+    private int gunlancer;
+    private int paladin;
+    private int slayer;
+    private int arcanist;
+    private int summoner;
+    private int bard;
+    private int sorceress;
+    private int wardancer;
+    private int scrapper;
+    private int soulfist;
+    private int glaivier;
+    private int striker;
+    private int breaker;
+    private int deathblade;
+    private int shadowhunter;
+    private int reaper;
+    private int souleater;
+    private int sharpshooter;
+    private int deadeye;
+    private int artillerist;
+    private int aeromancer;
+    private int machinist;
+    private int gunslinger;
+    private int artist;
+    private Timestamp createdAt;
 }
