e0c269b3fe856b62ad84094c7f5a1f6416607998
MOON1106 <mlnwoun8@gmail.com>
2024-09-09 20:35:35 +0900

    클라이언트에게 요청을 받고, 서비스클래스를 호출하여 데이터 저장

--- src/main/java/com/TeamNull/LostArk/LostArk/controller/UserController.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/controller/UserController.java
@@ -1,4 +1,31 @@
 package com.TeamNull.LostArk.LostArk.controller;
 
+import com.TeamNull.LostArk.LostArk.dto.UserDto;
+import com.TeamNull.LostArk.LostArk.entity.User;
+import com.TeamNull.LostArk.LostArk.repository.UserRepository;
+import com.TeamNull.LostArk.LostArk.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserRepository userRepository;
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UUID> createUser(@RequestBody UserDto userDto) {
+
+        //Entity의 객체를 생성
+        User saveUser = new User();
+        // User 객체를 저장
+        User savedUser = userService.saveUserd(userDto);
+        //User 객체를 응답으로 반환
+        return ResponseEntity.ok(savedUser.getId());
+    }
 }
