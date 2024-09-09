e7681afe9d521b33d08c36d7956ab61ef96b023d
qnak9740 <qnak9740@naver.com>
2024-09-09 17:14:13 +0900

    repository 파일 entity 와 연결

--- src/main/java/com/TeamNull/LostArk/LostArk/repository/ResultRepository.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/repository/ResultRepository.java
@@ -1,4 +1,7 @@
 package com.TeamNull.LostArk.LostArk.repository;
 
-public interface ResultRepository {
+import com.TeamNull.LostArk.LostArk.entity.Result;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResultRepository extends JpaRepository<Result, Integer> {
 }
