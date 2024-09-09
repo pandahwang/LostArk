377ebebf15173ffc9730bbfe29acd06d82f56ca4
MOON1106 <mlnwoun8@gmail.com>
2024-09-09 20:34:30 +0900

    JPA리포지터리와 연동

--- src/main/java/com/TeamNull/LostArk/LostArk/repository/UserRepository.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.TeamNull.LostArk.LostArk.repository;
 
-public interface UserRepository {
+import com.TeamNull.LostArk.LostArk.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
