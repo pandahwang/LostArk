23fe7e633757fe3826476db29f7d4be7e5eea853
MOON1106 <mlnwoun8@gmail.com>
2024-09-09 19:42:31 +0900

    Entity클래스 ID값 UUID타입 지정

--- src/main/java/com/TeamNull/LostArk/LostArk/dto/UserDto.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/dto/UserDto.java
@@ -1,4 +1,26 @@
 package com.TeamNull.LostArk.LostArk.dto;
 
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Setter
+@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
+
+    private UUID Id;
+    private int Question1;
+    private int Question2;
+    private int Question3;
+    private int Question4;
+    private int Question5;
+    private int Question6;
+    private int Question7;
+    private int Question8;
+    private int Question9;
+    private int Question10;
 }
