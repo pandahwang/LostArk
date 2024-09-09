8ad778e173f785c90654dae9450a84e7737cf231
MOON1106 <mlnwoun8@gmail.com>
2024-09-09 20:34:04 +0900

    entity 객체 생성 후 DTO값을 설정, 저장.

--- src/main/java/com/TeamNull/LostArk/LostArk/service/UserService.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/service/UserService.java
@@ -1,4 +1,34 @@
 package com.TeamNull.LostArk.LostArk.service;
 
+
+import com.TeamNull.LostArk.LostArk.dto.UserDto;
+import com.TeamNull.LostArk.LostArk.entity.User;
+import com.TeamNull.LostArk.LostArk.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User saveUserd(UserDto userDto){
+
+        User user = new User();
+        user.setQuestion1(userDto.getQuestion1());
+        user.setQuestion2(userDto.getQuestion2());
+        user.setQuestion3(userDto.getQuestion3());
+        user.setQuestion4(userDto.getQuestion4());
+        user.setQuestion5(userDto.getQuestion5());
+        user.setQuestion6(userDto.getQuestion6());
+        user.setQuestion7(userDto.getQuestion7());
+        user.setQuestion8(userDto.getQuestion8());
+        user.setQuestion9(userDto.getQuestion9());
+        user.setQuestion10(userDto.getQuestion10());
+
+        return userRepository.save(user);
+    }
+
+
 }
