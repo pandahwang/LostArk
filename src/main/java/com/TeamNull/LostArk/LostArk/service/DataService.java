86dd2d434df00da855cfd57ca5cabb35ef457904
easys <doit@easys.co.kr>
2024-09-09 18:23:17 +0900

    Optional 처리 개선

--- src/main/java/com/TeamNull/LostArk/LostArk/service/DataService.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/service/DataService.java
@@ -3,6 +3,7 @@ package com.TeamNull.LostArk.LostArk.service;
 import com.TeamNull.LostArk.LostArk.dto.DataDto;
 import com.TeamNull.LostArk.LostArk.entity.Data;
 import com.TeamNull.LostArk.LostArk.repository.DataRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,38 +16,38 @@ public class DataService {
     private final DataRepository dataRepository;
 
 
-    public DataDto findById(int id) {
-        Optional<Data> result = dataRepository.findById(id);
-        if (result.isPresent()) {
-            Data data = result.get();
-            DataDto dataDto = new DataDto();
-            dataDto.setId(data.getId());
-            dataDto.setBerserker(data.getBerserker());
-            dataDto.setDestroyer(data.getDestroyer());
-            dataDto.setGunlancer(data.getGunlancer());
-            dataDto.setPaladin(data.getPaladin());
-            dataDto.setArcanist(data.getArcanist());
-            dataDto.setSummoner(data.getSummoner());
-            dataDto.setBard(data.getBard());
-            dataDto.setSorceress(data.getSorceress());
-            dataDto.setWardancer(data.getWardancer());
-            dataDto.setScrapper(data.getScrapper());
-            dataDto.setSoulfist(data.getSoulfist());
-            dataDto.setGlaivier(data.getGlaivier());
-            dataDto.setStriker(data.getStriker());
-            dataDto.setDeathblade(data.getDeathblade());
-            dataDto.setShadowhunter(data.getShadowhunter());
-            dataDto.setReaper(data.getReaper());
-            dataDto.setSharpshooter(data.getSharpshooter());
-            dataDto.setDeadeye(data.getDeadeye());
-            dataDto.setArtillerist(data.getArtillerist());
-            dataDto.setMachinist(data.getMachinist());
-            dataDto.setGunslinger(data.getGunslinger());
-            dataDto.setAeromancer(data.getAeromancer());
-            dataDto.setSlayer(data.getSlayer());
-            dataDto.setCreatedAt(data.getCreatedAt());
-            return dataDto;
-        }
-        return null;
+    public DataDto findById(Integer id) {
+        Data data = dataRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Data not found for id: " + id));
+
+        DataDto dataDto = new DataDto();
+        dataDto.setId(data.getId());
+        dataDto.setBerserker(data.getBerserker());
+        dataDto.setDestroyer(data.getDestroyer());
+        dataDto.setGunlancer(data.getGunlancer());
+        dataDto.setPaladin(data.getPaladin());
+        dataDto.setArcanist(data.getArcanist());
+        dataDto.setSummoner(data.getSummoner());
+        dataDto.setBard(data.getBard());
+        dataDto.setSorceress(data.getSorceress());
+        dataDto.setWardancer(data.getWardancer());
+        dataDto.setScrapper(data.getScrapper());
+        dataDto.setSoulfist(data.getSoulfist());
+        dataDto.setGlaivier(data.getGlaivier());
+        dataDto.setStriker(data.getStriker());
+        dataDto.setDeathblade(data.getDeathblade());
+        dataDto.setShadowhunter(data.getShadowhunter());
+        dataDto.setReaper(data.getReaper());
+        dataDto.setSharpshooter(data.getSharpshooter());
+        dataDto.setDeadeye(data.getDeadeye());
+        dataDto.setArtillerist(data.getArtillerist());
+        dataDto.setMachinist(data.getMachinist());
+        dataDto.setGunslinger(data.getGunslinger());
+        dataDto.setAeromancer(data.getAeromancer());
+        dataDto.setSlayer(data.getSlayer());
+        dataDto.setCreatedAt(data.getCreatedAt());
+        return dataDto;
     }
+
+
 }
