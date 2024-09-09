7e121eac5f20a5c116fc05bd98a1d1e21f818a7e
pandahwang <pandahwang@naver.com>
2024-09-09 18:22:12 +0900

    alluser() api로부터 가져온 json 배열 데이터를 Map에 저장하는 기능 테스트

--- src/main/java/com/TeamNull/LostArk/LostArk/controller/OuterDataController.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/controller/OuterDataController.java
@@ -1,5 +1,7 @@
 package com.TeamNull.LostArk.LostArk.controller;
 
+import com.TeamNull.LostArk.LostArk.dto.OuterDataDto;
+import com.TeamNull.LostArk.LostArk.repository.OuterDataRepository;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -11,19 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
 @RequiredArgsConstructor
 public class OuterDataController {
 
+    private final OuterDataRepository outerDataRepository;
+
 
     // 목표 : Get 요청 시 api로부터 데이터를 받아와 저장 후 반환하도록 구현.
 
     @GetMapping("/statistic/alluser")
-    public JSONArray alluser() throws IOException, ParseException {
+    public Map<String, Integer> alluser() throws IOException, ParseException {
         String result;
         URL url = new URL("https://asia-northeast3-loasearch.cloudfunctions.net/app/total?minLv=&maxLv=");
         BufferedReader bf;
@@ -34,9 +39,20 @@ public class OuterDataController {
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonObject = (JSONObject)jsonParser.parse(result);
         JSONObject data = (JSONObject) jsonObject.get("data");
-        JSONArray classTotal = (JSONArray)data.get("classTotal");
+        JSONArray classTotalArr = (JSONArray)data.get("classTotal");
 
-        return classTotal;
+
+        Map<String, Integer> classTotalMap = new HashMap<>();
+        
+        // 받아온 JSON 배열을 classTotalMap에 저장
+        for (Object obj : classTotalArr){
+            JSONObject classData = (JSONObject) obj;
+            String className = (String) classData.get("className");
+            int classTotal = Integer.parseInt(classData.get("classTotal").toString());
+            classTotalMap.put(className, classTotal);
+        }
+
+        return classTotalMap;
     }
 
 
