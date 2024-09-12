8d1ffbdc97074eef09909517aaa731d5b06ba68c
dkwooje <50919538+dkwooje@users.noreply.github.com>
2024-09-12 17:51:11 +0900

    Service 댓글 저장 기능 만들기

--- src/main/java/com/TeamNull/LostArk/LostArk/service/CommentService.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/service/CommentService.java
@@ -1,4 +1,21 @@
 package com.TeamNull.LostArk.LostArk.service;
 
 public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+
+
+    public void commentAdd( String content,
+                            String password,
+                            User user)
+    {
+        Comment comment = new Comment();
+        comment.setContent(content);
+        comment.setPassword(password);
+        comment.setUser(user);
+        commentRepository.save(comment);
+    }
+
 }
+
