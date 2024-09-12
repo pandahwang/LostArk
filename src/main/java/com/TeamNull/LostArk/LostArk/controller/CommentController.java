146847c9f41324921f42a657040a32927ac6ce4a
dkwooje <50919538+dkwooje@users.noreply.github.com>
2024-09-12 17:52:39 +0900

    컨트롤러 만들기

--- src/main/java/com/TeamNull/LostArk/LostArk/controller/CommentController.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/controller/CommentController.java
@@ -1,4 +1,27 @@
 package com.TeamNull.LostArk.LostArk.controller;
 
+import com.TeamNull.LostArk.LostArk.dto.CommentDto;
+import com.TeamNull.LostArk.LostArk.entity.Comment;
+import com.TeamNull.LostArk.LostArk.entity.User;
+import com.TeamNull.LostArk.LostArk.repository.CommentRepository;
+import com.TeamNull.LostArk.LostArk.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/comment")
+@RequiredArgsConstructor
 public class CommentController {
 }
+
+    private final CommentService commentService;
+    private final CommentRepository commentRepository;
