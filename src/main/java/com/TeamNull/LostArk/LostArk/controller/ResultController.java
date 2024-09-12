55b6880c1d1ccbcb10aee60e3bc29bca633ca0fc
qnak9740 <qnak9740@naver.com>
2024-09-12 14:28:53 +0900

    테스트 결과 출력용 getmapping("result/{id}") 작성

--- src/main/java/com/TeamNull/LostArk/LostArk/controller/ResultController.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/controller/ResultController.java
@@ -2,12 +2,14 @@ package com.TeamNull.LostArk.LostArk.controller;
 
 import com.TeamNull.LostArk.LostArk.Job.JobAttributes;
 import com.TeamNull.LostArk.LostArk.dto.UserDto;
+import com.TeamNull.LostArk.LostArk.entity.Result;
 import com.TeamNull.LostArk.LostArk.entity.User;
 import com.TeamNull.LostArk.LostArk.repository.ResultRepository;
 import com.TeamNull.LostArk.LostArk.repository.UserRepository;
 import com.TeamNull.LostArk.LostArk.service.ResultService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,4 +29,14 @@ public class ResultController {
     public void top5 (@PathVariable UUID id){
         resultService.top5(id);
     }
+    @GetMapping("/result/{id}")
+    public ResponseEntity<Result> result(@PathVariable("id") int id) {
+        Optional<Result> result = resultRepository.findById(id);
+        if(result.isPresent()){
+            return new ResponseEntity<>(result.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
