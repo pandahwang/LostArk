6dbcfe2800f40f7bf6e87063c354949088792196
dkwooje <50919538+dkwooje@users.noreply.github.com>
2024-09-12 17:51:24 +0900

    Service 댓글 저장 기능 만들기 완료

--- src/main/java/com/TeamNull/LostArk/LostArk/service/CommentService.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/service/CommentService.java
@@ -1,5 +1,21 @@
 package com.TeamNull.LostArk.LostArk.service;
 
+
+import com.TeamNull.LostArk.LostArk.entity.Comment;
+import com.TeamNull.LostArk.LostArk.entity.User;
+import com.TeamNull.LostArk.LostArk.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.lang.module.ResolutionException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
 
     private final CommentRepository commentRepository;
