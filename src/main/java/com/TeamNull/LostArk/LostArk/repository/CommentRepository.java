5f32fefd367e34be762dc840b8a2a884a99665fe
dkwooje <50919538+dkwooje@users.noreply.github.com>
2024-09-12 17:51:50 +0900

    리포지토리 만들기

--- src/main/java/com/TeamNull/LostArk/LostArk/repository/CommentRepository.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/repository/CommentRepository.java
@@ -1,4 +1,10 @@
 package com.TeamNull.LostArk.LostArk.repository;
 
 public interface CommentRepository {
+
+import com.TeamNull.LostArk.LostArk.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface CommentRepository extends JpaRepository<Comment,Integer> {
 }
