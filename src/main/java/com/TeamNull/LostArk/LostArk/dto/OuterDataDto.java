c05c5c61e68fd360ba27a84c983e69acbfe54cb5
pandahwang <pandahwang@naver.com>
2024-09-12 14:42:56 +0900

    반환 객체 필드 수정 name, percentage, icon(링크)로 수정 + 매개변수가 있는 생성자 생성

--- src/main/java/com/TeamNull/LostArk/LostArk/dto/OuterDataDto.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/dto/OuterDataDto.java
@@ -4,38 +4,23 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.net.URL;
 import java.sql.Timestamp;
 
 @Getter
 @Setter
-@Builder
 public class OuterDataDto {
-    private int id;
-    private int berserker;
-    private int destroyer;
-    private int gunlancer;
-    private int paladin;
-    private int slayer;
-    private int arcanist;
-    private int summoner;
-    private int bard;
-    private int sorceress;
-    private int wardancer;
-    private int scrapper;
-    private int soulfist;
-    private int glaivier;
-    private int striker;
-    private int breaker;
-    private int deathblade;
-    private int shadowhunter;
-    private int reaper;
-    private int souleater;
-    private int sharpshooter;
-    private int deadeye;
-    private int artillerist;
-    private int aeromancer;
-    private int machinist;
-    private int gunslinger;
-    private int artist;
-    private Timestamp createdAt;
+    private String name;
+    private double percentage;
+    private String icon;
+
+    public OuterDataDto(String name, int value , Long total, String icon) {
+        double percentage =  (double) value / total * 100;
+        System.out.println("value:"+value);
+        System.out.println("total:"+total);
+        System.out.println("percentage:" + percentage);
+        this.name = name;
+        this.percentage = (double) (Math.round(percentage * 100) / 100.0);
+        this.icon = icon;
+    }
 }
