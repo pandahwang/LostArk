00168d52c0030bf0a17bc0dd0ca9724e201a449e
pandahwang <pandahwang@naver.com>
2024-09-19 18:12:44 +0900

    feat: User 객체를 반환하는 api 추가

--- src/main/java/com/TeamNull/LostArk/LostArk/controller/UserController.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/controller/UserController.java
@@ -4,6 +4,7 @@ import com.TeamNull.LostArk.LostArk.dto.UserDto;
 import com.TeamNull.LostArk.LostArk.entity.User;
 import com.TeamNull.LostArk.LostArk.repository.UserRepository;
 import com.TeamNull.LostArk.LostArk.service.UserService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,5 +25,11 @@ public class UserController {
         User newUser = userService.saveUser(userDto);
         //User 객체를 응답으로 반환
         return ResponseEntity.ok(newUser.getId());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable UUID id) {
+        // id로 User 객체를 찾아 반환
+        return ResponseEntity.ok(userRepository.findById(id).get());
     }
 }
