908b111f0b888ea211a65e43ced50060e51791cc
pandahwang <pandahwang@naver.com>
2024-09-30 11:55:03 +0900

    test: OuterDataController Alluser() 테스트 구문 추가

--- src/test/java/com/TeamNull/LostArk/LostArk/LostArkApplicationTests.java
+++ src/test/java/com/TeamNull/LostArk/LostArk/LostArkApplicationTests.java
@@ -1,13 +1,34 @@
 package com.TeamNull.LostArk.LostArk;
 
+import com.TeamNull.LostArk.LostArk.controller.OuterDataController;
+import com.TeamNull.LostArk.LostArk.dto.OuterDataDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class LostArkApplicationTests {
 
+	private OuterDataController outerDataController;
+
+	@Autowired
+	public LostArkApplicationTests(OuterDataController outerDataController) {
+		super();
+		this.outerDataController = outerDataController;
+	}
+
 	@Test
-	void contextLoads() {
+	void testAlluser() {
+		List<OuterDataDto> outerDataDtoList = this.outerDataController.alluser();
+		assertEquals(26, outerDataDtoList.size());
+		assertEquals("도화가", outerDataDtoList.get(0).getName());
+		System.out.println(outerDataDtoList.get(0).getName());
+		System.out.println(outerDataDtoList.get(0).getValue());
+		System.out.println(outerDataDtoList.get(0).getIcon());
 	}
 
 }
