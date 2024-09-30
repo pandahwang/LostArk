bc41616b4a2ba72dad5c83acd89191e414efb2ce
pandahwang <pandahwang@naver.com>
2024-09-30 12:17:41 +0900

    refactor: 전체 직업통계 업데이트 기능 switch문을 forEach문으로 간결하게 수정

--- src/main/java/com/TeamNull/LostArk/LostArk/service/OuterDataService.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/service/OuterDataService.java
@@ -89,59 +89,21 @@ public class OuterDataService {
             String className = (String) classData.get("className");
             int classTotal = Integer.parseInt(classData.get("classTotal").toString());
 
-            switch (className) {
-                case "디스트로이어" : outerData.setDestroyer(classTotal);
-                    break;
-                case "버서커" : outerData.setBerserker(classTotal);
-                    break;
-                case "슬레이어" : outerData.setSlayer(classTotal);
-                    break;
-                case "워로드" : outerData.setGunlancer(classTotal);
-                    break;
-                case "홀리나이트" : outerData.setPaladin(classTotal);
-                    break;
-                case "기공사" : outerData.setSoulfist(classTotal);
-                    break;
-                case "배틀마스터" : outerData.setWardancer(classTotal);
-                    break;
-                case "브레이커" : outerData.setBreaker(classTotal);
-                    break;
-                case "스트라이커" : outerData.setStriker(classTotal);
-                    break;
-                case "인파이터" : outerData.setScrapper(classTotal);
-                    break;
-                case "창술사" : outerData.setGlaivier(classTotal);
-                    break;
-                case "건슬링어" : outerData.setGunslinger(classTotal);
-                    break;
-                case "데빌헌터" : outerData.setDeadeye(classTotal);
-                    break;
-                case "블래스터" : outerData.setArtillerist(classTotal);
-                    break;
-                case "스카우터" : outerData.setMachinist(classTotal);
-                    break;
-                case "호크아이" : outerData.setSharpshooter(classTotal);
-                    break;
-                case "바드" : outerData.setBard(classTotal);
-                    break;
-                case "서머너" : outerData.setSummoner(classTotal);
-                    break;
-                case "소서리스" : outerData.setSorceress(classTotal);
-                    break;
-                case "아르카나" : outerData.setArcanist(classTotal);
-                    break;
-                case "데모닉" : outerData.setShadowhunter(classTotal);
-                    break;
-                case "리퍼" : outerData.setReaper(classTotal);
-                    break;
-                case "블레이드" : outerData.setDeathblade(classTotal);
-                    break;
-                case "소울이터" : outerData.setSouleater(classTotal);
-                    break;
-                case "기상술사" : outerData.setAeromancer(classTotal);
-                    break;
-                case "도화가" : outerData.setArtist(classTotal);
-                    break;
+            // className과 고정 데이터의 jobName이 일치하는 key값 찾기
+            String key = jobAttributesMap.entrySet().stream()
+                    .filter(entry -> entry.getValue().getJobName().equals(className))
+                    .map(Map.Entry::getKey)
+                    .findFirst()
+                    .orElse(null);
+
+            if (key != null) {
+                try {
+                    String methodName = "set" + key.substring(0, 1).toUpperCase() + key.substring(1);
+                    Method method = outerData.getClass().getMethod(methodName, int.class);
+                    method.invoke(outerData, classTotal);
+                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
             }
         }
         outerData.setCreatedAt(new Timestamp(System.currentTimeMillis()));
