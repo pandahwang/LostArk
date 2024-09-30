8bcfdd9e77c84efda62837e825e95241f5e9e7e4
pandahwang <pandahwang@naver.com>
2024-09-30 16:12:08 +0900

    test: ResultController 결과 조회 기능 테스트 구문 추가

--- src/test/java/com/TeamNull/LostArk/LostArk/LostArkApplicationTests.java
+++ src/test/java/com/TeamNull/LostArk/LostArk/LostArkApplicationTests.java
@@ -2,15 +2,19 @@ package com.TeamNull.LostArk.LostArk;
 
 import com.TeamNull.LostArk.LostArk.controller.DataController;
 import com.TeamNull.LostArk.LostArk.controller.OuterDataController;
+import com.TeamNull.LostArk.LostArk.controller.ResultController;
 import com.TeamNull.LostArk.LostArk.dto.DataDto;
 import com.TeamNull.LostArk.LostArk.dto.OuterDataDto;
+import com.TeamNull.LostArk.LostArk.dto.ResultDto;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,12 +23,14 @@ class LostArkApplicationTests {
 
 	private OuterDataController outerDataController;
 	private DataController dataController;
+	private ResultController resultController;
 
 	@Autowired
-	public LostArkApplicationTests(OuterDataController outerDataController, DataController dataController) {
+	public LostArkApplicationTests(OuterDataController outerDataController, DataController dataController, ResultController resultController) {
 		super();
 		this.outerDataController = outerDataController;
 		this.dataController = dataController;
+		this.resultController = resultController;
 	}
 
 //	@Test
@@ -42,13 +48,24 @@ class LostArkApplicationTests {
 //		this.outerDataController.createAlluser();
 //	}
 
+//	@Test
+//	void testData() {
+//		List<DataDto> dataDtoList = this.dataController.read();
+//		assertEquals(26, dataDtoList.size());
+//		System.out.println(dataDtoList.get(0).getName());
+//		System.out.println(dataDtoList.get(0).getValue());
+//		System.out.println(dataDtoList.get(0).getIcon());
+//	}
+
+//	@Test
+//	void testTop5() {
+//		this.resultController.top5(UUID.fromString("f9ceff67-e36b-43d9-89a8-263a334a6313"));
+//	}
+
 	@Test
-	void testData() {
-		List<DataDto> dataDtoList = this.dataController.read();
-		assertEquals(26, dataDtoList.size());
-		System.out.println(dataDtoList.get(0).getName());
-		System.out.println(dataDtoList.get(0).getValue());
-		System.out.println(dataDtoList.get(0).getIcon());
+	void testResult() {
+		ResponseEntity<List<ResultDto>> result = this.resultController.result(UUID.fromString("ffb057b9-0b82-4789-8440-ded61c248cd2"));
+		System.out.println(result.getBody().get(0).getName());
 	}
 
 }
