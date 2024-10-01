1d17f6f48683354a1fc74a5b6e2301a2fb141b41
pandahwang <pandahwang@naver.com>
2024-10-01 20:44:19 +0900

    add: WebConfig CORS 설정 프론트엔드 IP 추가

--- src/main/java/com/TeamNull/LostArk/LostArk/WebConfig.java
+++ src/main/java/com/TeamNull/LostArk/LostArk/WebConfig.java
@@ -11,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:3000"
-                        // front IP 주소 추가할 곳
+                        "http://localhost:3000",
+                        "http://52.79.232.220:3000"// front IP 주소 추가할 곳""
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
